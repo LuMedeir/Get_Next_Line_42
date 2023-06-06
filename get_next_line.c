56273a8c6e9cd6b422894e585bd192f1068ebd6a
Luiza Medeiros <126430433+LuMedeir@users.noreply.github.com>
2023-06-06 11:15:32 -0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -52,17 +52,17 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-int main(void)
-{
-    int fd;
-    char    *line;
+// int main(void)
+// {
+//     int fd;
+//     char    *line;
 
-    fd = open("test.txt", O_RDONLY);
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s",line);
-        free(line);
-    }
-    close(fd);
-    return (0);
-}
+//     fd = open("test.txt", O_RDONLY);
+//     while ((line = get_next_line(fd)) != NULL)
+//     {
+//         printf("%s",line);
+//         free(line);
+//     }
+//     close(fd);
+//     return (0);
+// }
